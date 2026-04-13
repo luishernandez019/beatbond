6c1422bf2a0d64418d5053af5548fdd06585bde9
Luis Ordóñez <luishdz.dev@gmail.com>
2026-04-13 00:19:44 -0600

    fix: remove unnecessary eslint-disable comment in useRecommendations hook feat: update eslint configuration to disable specific React and Next.js rules

--- src/hooks/useRecommendations.ts
+++ src/hooks/useRecommendations.ts
@@ -91,7 +91,6 @@ export function useRecommendations(
     } finally {
       setLoading(false);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function handleLike() {
